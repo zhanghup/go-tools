e50013943b66500cdeeb6df7d50aebaaf026cc83
unknown <zhang3611@qq.com>
2020-01-14 21:22:26 +0800

    sync

--- common.go
+++ common.go
@@ -4,19 +4,54 @@ import (
 	"bytes"
 	"crypto/md5"
 	"crypto/sha256"
+	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"gopkg.in/mgo.v2/bson"
+	"math/rand"
 	"os"
 	"strings"
+	"sync/atomic"
 	"text/template"
 	"time"
 )
 
-func ObjectString() *string {
-	str := bson.NewObjectId().Hex()
-	return &str
+func ObjectId() string {
+	machineId := func() []byte {
+		var sum [3]byte
+		id := sum[:]
+		hostname, err1 := os.Hostname()
+		if err1 != nil {
+			n := uint32(time.Now().UnixNano())
+			sum[0] = byte(n >> 0)
+			sum[1] = byte(n >> 8)
+			sum[2] = byte(n >> 16)
+			return id
+		}
+		hw := md5.New()
+		hw.Write([]byte(hostname))
+		copy(id, hw.Sum(nil))
+		return id
+	}()
+	processId := os.Getpid()
+	objectIdCounter := uint32(time.Now().UnixNano())
+
+	var b [12]byte
+	// Timestamp, 4 bytes, big endian
+	binary.BigEndian.PutUint32(b[:], uint32(time.Now().Unix()))
+	// Machine, first 3 bytes of md5(hostname)
+	b[4] = machineId[0]
+	b[5] = machineId[1]
+	b[6] = machineId[2]
+	// Pid, 2 bytes, specs don't specify endianness, but we use big endian.
+	b[7] = byte(processId >> 8)
+	b[8] = byte(processId)
+	// Increment, 3 bytes, big endian
+	i := atomic.AddUint32(&objectIdCounter, 1)
+	b[9] = byte(i >> 16)
+	b[10] = byte(i >> 8)
+	b[11] = byte(i)
+	return hex.EncodeToString(b[:])
 }
 
 // 以json格式输出struct对象
@@ -67,25 +102,21 @@ func TimeToYear() string {
 func PtrInt(i int) *int {
 	return &i
 }
-
 func PtrString(i string) *string {
 	return &i
 }
-
 func PtrInt64(i int64) *int64 {
 	return &i
 }
-
 func PtrFloat64(i float64) *float64 {
 	return &i
 }
-
 func PtrInterface(i interface{}) *interface{} {
 	return &i
 }
 
 func StrTemplate(str string, format map[string]interface{}, funcMap template.FuncMap) (string, error) {
-	tt := template.New(bson.NewObjectId().Hex())
+	tt := template.New(ObjectId())
 	fmap := template.FuncMap{
 		"title": strings.Title,
 	}
@@ -103,7 +134,6 @@ func StrTemplate(str string, format map[string]interface{}, funcMap template.Fun
 	err = tt.Execute(buf, format)
 	return buf.String(), err
 }
-
 func StrContains(src []string, tag string) bool {
 	for _, s := range src {
 		if s == tag {
@@ -111,4 +141,14 @@ func StrContains(src []string, tag string) bool {
 		}
 	}
 	return false
+}
+
+func RanderString(l int) string {
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	res := make([]byte, l)
+	for i := 0; i < l; i++ {
+		b := r.Intn(26) + 65
+		res[i] = byte(b)
+	}
+	return string(res)
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/zhanghup/go-tools
 
 go 1.12
-
-require gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
-
-replace gopkg.in/mgo.v2 => github.com/zhanghup/go-mgo v1.0.0
