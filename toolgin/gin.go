15b37b6b478dd901bafedea6dff6f7d0083bf887
zhanghup <zhang3611@qq.com>
2020-04-06 13:50:12 +0800

    sync

--- toolgin/gin.go
+++ toolgin/gin.go
@@ -9,3 +9,31 @@ type Config struct {
 func NewGin(cfg Config) *gin.Engine {
 	return gin.Default()
 }
+
+func Do(c *gin.Context, fn func(c *gin.Context) (interface{}, string)) {
+	o, err := fn(c)
+	if len(err) != 0 {
+		c.JSON(200, map[string]interface{}{
+			"code":     -1,
+			"msg":      err,
+			"response": o,
+		})
+	} else {
+		c.JSON(200, map[string]interface{}{
+			"code":     0,
+			"msg":      "ok",
+			"response": o,
+		})
+	}
+}
+
+func DoCustom(c *gin.Context, fn func(c *gin.Context) (interface{}, string)) {
+	o, err := fn(c)
+	if len(err) != 0 {
+		c.JSON(200, map[string]interface{}{
+			"code":     -1,
+			"msg":      err,
+			"response": o,
+		})
+	}
+}
