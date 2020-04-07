2021711e902745650131c370c24bfe0aa89f269f
zhanghup <zhang3611@qq.com>
2020-04-07 20:15:48 +0800

    sync

--- export_rft.go
+++ export_rft.go
@@ -1,6 +1,9 @@
 package tools
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 type myrft struct{}
 
@@ -17,4 +20,46 @@ func (this myrft) realValue(o reflect.Value) reflect.Value {
 		return this.realValue(o)
 	}
 	return o
+}
+
+func (this myrft) DeepSet(o interface{}, fn func(t reflect.Type, v reflect.Value, tf reflect.StructField) bool) {
+	ty := reflect.TypeOf(o)
+	vl := reflect.ValueOf(o)
+	if ty.Kind() != reflect.Ptr {
+		panic("DeepSet 输入必须为指针")
+	}
+	ty = ty.Elem()
+	vl = vl.Elem()
+	this.deepSet(ty, vl, reflect.StructField{}, fn)
+}
+
+func (this myrft) deepSet(ty reflect.Type, vl reflect.Value, tf reflect.StructField, fn func(t reflect.Type, v reflect.Value, tf reflect.StructField) bool) {
+	switch ty.Kind() {
+	case reflect.Ptr:
+		if !vl.CanSet() {
+			return
+		}
+		if vl.Pointer() == 0 {
+			if !fn(ty, vl, tf) {
+				return
+			}
+		}
+		ty = ty.Elem()
+		vl = vl.Elem()
+		this.deepSet(ty, vl, tf, fn)
+	case reflect.Struct:
+		for i := 0; i < vl.NumField(); i++ {
+			tf := ty.Field(i)
+			v := vl.Field(i)
+			t := tf.Type
+			fmt.Println(v.CanSet())
+			this.deepSet(t, v, tf, fn)
+		}
+	default:
+		if !vl.CanSet() {
+			return
+		}
+		fn(ty, vl, tf)
+	}
+
 }
