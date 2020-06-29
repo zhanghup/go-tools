21195d5cf809714cca16f529bfbf0ec4099129e9
张超 <zhang3611@qq.com>
2020-06-29 17:43:01 +0800

    sync

---
+++ tgql/test/gql_test.go
@@ -0,0 +1,26 @@
+package main_test
+
+import (
+	"github.com/99designs/gqlgen/client"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/zhanghup/go-tools/tgql/test/graph"
+	"github.com/zhanghup/go-tools/tgql/test/graph/generated"
+	"testing"
+)
+
+func TestName(t *testing.T) {
+
+	c := client.New(handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}})))
+	res := map[string]interface{}{}
+	c.MustPost(`
+	query Todo{
+	todos{
+		id
+		text
+		done
+		user{id name}
+	}
+}
+`, &res)
+
+}
