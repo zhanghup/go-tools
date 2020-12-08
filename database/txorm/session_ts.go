aa1af8cba1e5eb14092cbcb5686474d6910ad9f0
张超 <zhang3611@qq.com>
2020-12-08 15:17:28 +0800

    sync

--- database/txorm/session_ts.go
+++ database/txorm/session_ts.go
@@ -12,6 +12,12 @@ func (this *Session) TS(fn func(sess ISession) error, commit ...bool) error {
 	}
 	if len(commit) > 0 && commit[0] {
 		return this.Commit()
+	} else if this.autoClose {
+		err := this.Commit()
+		if err != nil {
+			return err
+		}
+		return this.Close()
 	}
 	return nil
 }
