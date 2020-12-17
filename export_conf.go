bd49a6fe9533c7cb2f7dbe9fd6b0884eeb152460
zhanghup <zhang3611@qq.com>
2020-12-17 20:54:28 +0800

    sync

--- export_conf.go
+++ export_conf.go
@@ -7,9 +7,11 @@ package tools
 
 import (
 	"errors"
+	"fmt"
 	rice "github.com/GeertJohan/go.rice"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"os"
 )
 
 func Conf(box *rice.Box, data interface{}) error {
@@ -22,14 +24,24 @@ func Conf(box *rice.Box, data interface{}) error {
 		return errors.New(Str.Fmt(`config.yml - %s - err: %s`, s, err.Error()))
 	}
 
-	f, err := box.Open("config.yml")
+	f, err := os.Open(fmt.Sprintf("%s/%s", box.Name(), "config.yml"))
+	datas := make([]byte, 0)
 	if err != nil {
-		return exception("配置文件文件打开失败", err)
+		f, err := box.Open("config.yml")
+		if err != nil {
+			return exception("[1] 配置文件文件打开失败", err)
+		}
+		datas, err = ioutil.ReadAll(f)
+		if err != nil {
+			return exception("[2] 配置文件文件读取失败", err)
+		}
+	} else {
+		datas, err = ioutil.ReadAll(f)
+		if err != nil {
+			return exception("[3] 配置文件文件读取失败", err)
+		}
 	}
-	datas, err := ioutil.ReadAll(f)
-	if err != nil {
-		return exception("配置文件文件读取失败", err)
-	}
+
 	if err := yaml.Unmarshal(datas, data); err != nil {
 		return exception("yaml 格式化失败", err)
 	}
