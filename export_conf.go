919c0b9d921996f6c7f2328c88d8359316575440
zhanghup <zhang3611@qq.com>
2021-10-24 21:23:11 +0800

    sync

--- export_conf.go
+++ export_conf.go
@@ -6,72 +6,9 @@ package tools
 */
 
 import (
-	"embed"
 	"errors"
-	"fmt"
-	rice "github.com/GeertJohan/go.rice"
 	"gopkg.in/yaml.v2"
-	"io/ioutil"
-	"os"
 )
-
-func Conf(box *rice.Box, data interface{}) error {
-	err := PtrCheck(data)
-	if err != nil {
-		return err
-	}
-
-	exception := func(s string, err error) error {
-		return errors.New(StrFmt(`config.yml - %s - err: %s`, s, err.Error()))
-	}
-
-	f, err := os.Open(fmt.Sprintf("%s/%s", box.Name(), "config.yml"))
-	datas := make([]byte, 0)
-	if err != nil {
-		f, err := box.Open("config.yml")
-		if err != nil {
-			return exception("[1] 配置文件文件打开失败", err)
-		}
-		datas, err = ioutil.ReadAll(f)
-		if err != nil {
-			return exception("[2] 配置文件文件读取失败", err)
-		}
-	} else {
-		datas, err = ioutil.ReadAll(f)
-		if err != nil {
-			return exception("[3] 配置文件文件读取失败", err)
-		}
-	}
-
-	return ConfOfByte(datas, data)
-}
-
-func ConfOfEmbed(localPath string, ebd embed.FS, data interface{}) error {
-	err := PtrCheck(data)
-	if err != nil {
-		return err
-	}
-
-	exception := func(s string, err error) error {
-		return errors.New(StrFmt(`config.yml - %s - err: %s`, s, err.Error()))
-	}
-
-	f, err := os.Open(localPath)
-	datas := make([]byte, 0)
-	if err != nil {
-		datas, err = ebd.ReadFile("config.yml")
-		if err != nil {
-			return exception("[1] 配置文件文件读取失败", err)
-		}
-	} else {
-		datas, err = ioutil.ReadAll(f)
-		if err != nil {
-			return exception("[2] 配置文件文件读取失败", err)
-		}
-	}
-
-	return ConfOfByte(datas, data)
-}
 
 func ConfOfByte(dataByte []byte, data interface{}) error {
 	exception := func(s string, err error) error {
--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.17
 require (
 	github.com/360EntSecGroup-Skylar/excelize/v2 v2.3.1
 	github.com/99designs/gqlgen v0.13.0
-	github.com/GeertJohan/go.rice v1.0.1
 	github.com/PuerkitoBio/goquery v1.5.1
 	github.com/gin-gonic/gin v1.7.3
 	github.com/go-resty/resty/v2 v2.4.0
@@ -19,19 +18,4 @@ require (
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/yaml.v2 v2.2.8
 	xorm.io/xorm v1.2.2
-)
-
-require (
-	github.com/daaku/go.zipexe v1.0.0 // indirect
-	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-playground/locales v0.13.0 // indirect
-	github.com/go-playground/universal-translator v0.17.0 // indirect
-	github.com/go-playground/validator/v10 v10.4.1 // indirect
-	github.com/golang/protobuf v1.3.3 // indirect
-	github.com/leodido/go-urn v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
-	github.com/ugorji/go/codec v1.1.7 // indirect
-	go.uber.org/atomic v1.6.0 // indirect
-	go.uber.org/multierr v1.5.0 // indirect
-	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
 )
